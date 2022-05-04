e384a47ca7c563b6dc4905fff3b84b3edb94c246
Fabian Caamal Haas <50337714+FabianCaamal@users.noreply.github.com>
2022-05-04 10:07:41 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/grupos/nuevo', 'api\angular@store');				// cread
-Route::get('/grupos', 'api\angular@index');		    			// read
-Route::post('/grupos/{id}/update', 'api\angular@update');		// update
-Route::delete('/grupos/{id}/delete', 'api\angular@destroy');		// delete
-
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
